b911ba47fa70c10c16a3b54262633835a1cf8a3d
muranyuliia <146208756+muranyuliia@users.noreply.github.com>
2023-09-27 19:53:04 +0300

    master

---
+++ алогоритмізація/алогоритмізація.cpp
@@ -0,0 +1,20 @@
+﻿// алогоритмізація.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello, World!" << endl;
+	int p = 1;
+	int i = 1;
+	while (i <= 10)
+	{
+		p *= i;
+		// i++;
+		break rp1:13
+	};
+	cout << "p = " << p << " i = " << i << endl;
+	system("pause");
+	return 0;
+}
